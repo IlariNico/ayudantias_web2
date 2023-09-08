7aead4b452e384cec426a91920a74f6fc4447c79
Lulo1997 <oroquietaluciano@gmail.com>
2023-09-08 15:04:11 -0300

    agrego tp3 roto

---
+++ Lulo/Tp3/home.php
@@ -0,0 +1,136 @@
+<html>
+<head>
+</head>
+<body>
+<div class="container">
+
+<?php
+$db = new PDO('mysql:host=localhost;'.'dbname=db_deudas;charset=utf8', 'root', '');
+
+function getPagos($db){
+    //1. abrir la conexion con PDO
+   // $db = new PDO('mysql:host=localhost;' . 'dbname=db_deudas;charset=utf8', 'root', '' );
+
+    //2. ejecutar consulta SQL() (2 Subpasos: prepare y execute)
+    // prepare seria escribir la consulta y execute la ejecuta
+    $query = $db->prepare('SELECT * FROM pagos');
+    $query->execute();
+
+    //3. obtener datos de la consulta
+    $pays = $query->fetchAll(PDO::FETCH_OBJ); //devuelve un arreglo con todos los pagos (array de objetos).
+    //fetch devuelve un registro solo
+
+    return $pays;
+}
+//echo "<pre>";
+//var_dump($pay);
+//echo "</pre>";
+/*
+ echo "<br>";
+    echo "<p> Deudor: $i->deudor </p>";
+    echo "<p> Numero de cuota: $i->cuota </p>";
+    echo "<p> Monto de cuota: $i->cuota_capital </p>";
+    echo "<p> Fecha de pago: $i->fecha_pago </p>";
+}
+*/ 
+
+
+function getDeudor($id){
+    $db = new PDO('mysql:host=localhost;' . 'dbname=db_deudas;charset=utf8', 'root', '' );
+     $query = $db->prepare('SELECT * FROM pagos WHERE id=?' );
+     $query->execute([$id]);
+     $pay = $query->fetch(PDO::FETCH_OBJ);
+     return $pay;
+}
+
+
+function createDeudor(){
+    GLOBAL $db;
+    $query="INSERT INTO deudas( deudor, cuota, cuota_capital, fecha_pago) VALUES (?,?,?,?)";
+    $sentence= $db->prepare($query);
+    $sentence-> execute([$_POST['deudor'],$_POST['cuota'],$_POST['cuota_capital']], $_POST['fecha_pago']);
+    header("Location: ". BASE_URL ."home");
+}
+
+function deleteDeudor($id){
+    GLOBAL $db;
+    $query="DELETE FROM pagos WHERE id=?";
+    $sentence= $db->prepare($query);
+    $sentence-> execute([$id]);
+    header("Location: ". BASE_URL ."home");
+}
+
+function showhome(){
+    global $db;
+    $pays=getPagos($db);
+    echo "<style>
+    table {
+        border-collapse: collapse;
+        width: 100%;
+    }
+
+    th, td {
+        padding: 8px;
+        text-align: left;
+        border-bottom: 1px solid #ddd;
+    }
+
+    th {
+        background-color: #f2f2f2;
+    }
+</style>";
+
+echo "<table>
+    <thead>
+        <tr>
+            <th>Id</th>
+            <th>deudor</th>
+            <th>cuota</th>
+            <th>cuota_capital</th>
+            <th>fecha_pago</th>
+        </tr>
+    </thead>
+    <tbody>";
+
+    echo "<form method='POST' action='crearDeudor'>
+        <label>Deudor</label>
+        <input type='text' name='Deudor' '></input>
+        <label>cuota</label>
+        <input type='text' name='cuota'></input>
+        <label>cuota_capital</label>
+        <input type='text' name='cuota_capital'></input>
+        <label>fecha_pago</label>
+        <input type='text' name='fecha_pago'></input>
+        <input type='submit' value='crear'></input>
+        </form>";
+        
+
+foreach($pays as $i){
+    echo "<tr>
+    <td>" . $i->id . "</td>
+    <td>" . $i->deudor . "</td>
+    <td>" . $i->cuota . "</td>
+    <td>" . $i->cuota_capital . "</td>
+    <td>" . $i->fecha_pago . "</td>
+    <td>
+    <a href='eliminar/" . $i->id . "'>Eliminar</a>
+       
+    </td>
+</tr>";
+
+    } 
+
+   
+}
+ShowHome();
+
+
+?>
+</div>
+</body>
+</html>
+
+
+
+
+
---
+++ Lulo/Tp3/routerr.php
@@ -0,0 +1,35 @@
+<?php
+
+require_once 'deudas.php';
+
+
+define('BASE_URL', '//'.$_SERVER['SERVER_NAME'] . ':' . $_SERVER['SERVER_PORT'] . dirname($_SERVER['PHP_SELF']).'/');
+
+// Lee la acción
+if (!empty($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'home'; // Acción por defecto si no se envía
+}
+
+// Parsea la acción Ej: tabla/5 --> ['tabla', 5]
+$params = explode('/', $action);
+
+// Determina qué camino seguir según la acción
+switch ($params[0]) {
+    case 'home':
+        showHome();
+        break;
+    case 'deudor':
+        getDeudor($params[1]);
+        break;    
+    case 'crear':
+        createDeudor();
+        break;
+    case 'elimiar':
+        deleteDeudor($params[1]);
+        break;    
+      
+}
+
+?>
